5cb81382a332595b1892962d181b57cc7d35173a
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-09-01 10:43:42 +0700

    Init project

---
+++ src/test/java/com/example/doctruyen/DocTruyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctruyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocTruyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
